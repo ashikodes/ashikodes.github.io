600faa00af7bd5e83dbb36f7cdeb02efc9b23392
Onye Ashikodi <ashikodiemeka@gmail.com>
2016-06-09 11:53:24 +0100

    add port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var app = require('./config/express')();
+var app = require('./config/express')(),
+    port = process.env.PORT || 3300;
 
-app.listen( 3300, function (err) {
+app.listen( port, function (err) {
   if (err) {
     return err;
   }
-  console.log('App started on port: ' + 3300);
+  console.log('App started on port: ' + port);
 });
 
 module.exports = app;
